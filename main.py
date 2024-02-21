685f10bc00c8fa34c41f370c235e2cdd4c47f3fb
omerterci <bengi.celik@ozu.edu.tr>
2024-02-22 01:08:25 +0300

    create onnx session for yolo model

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException, Query
 from fastapi.responses import JSONResponse
 import uvicorn
+import io
 
 app = FastAPI()
 
+model_path = os.path.join("Macintosh HD", os.sep, "Users", "beng", "Desktop", "case", "yolov5s.onnx")
+ort_session = ort.InferenceSession(model_path)
